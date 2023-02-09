35b611ec7a66ca1cd605d49629a4eee8743e7984
maven336 <mavenquijano36@gmail.com>
2023-02-09 15:11:08 +0800

    uploaded crud8info modify

---
+++ resources/views/contacts/edit.blade.php
@@ -0,0 +1,54 @@
+@extends('contacts.layout')
+   
+@section('content')
+    <div class="row">
+        <div class="col-lg-12 margin-tb">
+            <div class="pull-left">
+                <h2>Edit Contact</h2>
+            </div>
+            <div class="pull-right">
+                <a class="btn btn-primary" href="{{ route('contacts.index') }}"> Back</a>
+            </div>
+        </div>
+    </div>
+   
+    @if ($errors->any())
+        <div class="alert alert-danger">
+        There were some problems with your input.<br><br>
+            <ul>
+                @foreach ($errors->all() as $error)
+                    <li>{{ $error }}</li>
+                @endforeach
+            </ul>
+        </div>
+    @endif
+  
+    <form action="{{ route('contacts.update',$contact->id) }}" method="POST">
+        @csrf
+        @method('PUT')
+   
+         <div class="row">
+            <div class="col-xs-12 col-sm-12 col-md-12">
+                <div class="form-group">
+                    <strong>Name:</strong>
+                    <input type="text" name="name" value="{{ $contact->name }}" class="form-control" placeholder="Name">
+                </div>
+            </div>
+            <div class="col-xs-12 col-sm-12 col-md-12">
+                <div class="form-group">
+                    <strong>Address:</strong>
+                    <input type="text" name="address" value="{{ $contact->address }}" class="form-control" placeholder="Address">
+                </div>
+                <div class="col-xs-12 col-sm-12 col-md-12">
+                <div class="form-group">
+                    <strong>Telephone:</strong>
+                    <input type="text" name="mobile" value="{{ $contact->mobile }}" class="form-control" placeholder="Telephone">
+                </div>
+            </div>
+            <div class="col-xs-12 col-sm-12 col-md-12 text-center">
+              <button type="submit" class="btn btn-primary">Submit</button>
+            </div>
+        </div>
+   
+    </form>
+@endsection
